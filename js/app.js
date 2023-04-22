848d6c3c13d05747d315b7ed5e5e42d7da430223
Antahkarana98 <Antahkarana98@github.com>
2023-04-22 16:43:44 -0500

    pruebas con un  carusel cuaquiera ahora a vincularlo con el de lapagina

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ import { ratingCalc, formattTags } from './functions.js';
     const products = await apiQuery();
 
     const productsList = document.querySelector('#list-products');
-    
+
     products.forEach(product => {
       const { title, totalInventory, prices: {max: {amount: amountMax }, min: {amount: amountMin }}, featuredImage: { url:url }, tags } = product;
 
@@ -61,5 +61,72 @@ import { ratingCalc, formattTags } from './functions.js';
     });
   }
 
+
+  const carousel = document.querySelector('.carousel');
+  const btnArrows = document.querySelectorAll('.wrapper i');
+  const firstCardWidth = document.querySelector('.card2').offsetWidth;
+  const carouselChildrens = [...carousel.children];
+
+  let isDraging = false, startX, starScrollLeft;
+
+  // obtine el numero de card que se adptan al carrusel
+  let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
+
+  // agrega las card al inicio del carrusel para el loop infinito
+  carouselChildrens.slice(-cardPerView).reverse().forEach(card =>{
+    carousel.insertAdjacentHTML('afterbegin', card.outerHTML);
+  })
+
+  // agrega las card al final del carrusel para el loop infinito
+  carouselChildrens.slice(0, cardPerView).forEach(card =>{
+    carousel.insertAdjacentHTML('beforeend', card.outerHTML);
+  })
+
+  // agregar el evento a los botoones del scroll
+  btnArrows.forEach(btn => {
+    btn.addEventListener('click', () => {
+      carousel.scrollLeft += btn.id === 'left' ? -firstCardWidth : firstCardWidth;
+    })
+  });
+
+  const dragStart = (e) => {
+    isDraging = true;
+    carousel.classList.add('dragging');
+    //Guarda la posicion inicial del mouse y del scroll del carrusel
+    startX = e.pageX;
+    starScrollLeft = carousel.scrollLeft;
+  }
+
+  const dragging = (e) => {
+    if(!isDraging) return; //si draggring es falso lo retorna
+    // actualiza el scroll del carrusel basado en el movimiento del cursor
+    carousel.scrollLeft = starScrollLeft - (e.pageX - startX);
+  }
+
+  const dragEnd = () => {
+    isDraging = false;
+    carousel.classList.remove('dragging');
+  }
+
+  const infinteCarousel = () => {
+    // si el scroll llega al inicio lo devuelve al final
+    // se puso 40 para poder detectar el inicio ya que no llega completamente a 0
+    if(carousel.scrollLeft === 40){
+      carousel.classList.add('no-transition');
+      carousel.scrollLeft = carousel.scrollWidth - (2 * carousel.offsetWidth);
+      carousel.classList.remove('no-transition');
+    }
+    // si el scroll llega al final lo devuelve al inicio
+    else if(Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth){
+      carousel.classList.add('no-transition');
+      carousel.scrollLeft = carousel.offsetWidth;
+      carousel.classList.remove('no-transition');
+    }
+  }
+
+  carousel.addEventListener('mousedown', dragStart);
+  carousel.addEventListener('mousemove', dragging);
+  carousel.addEventListener('mouseup', dragEnd);
+  carousel.addEventListener('scroll', infinteCarousel);
 
 })();
