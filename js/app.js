c091d6323ebb29237d88db1160e8e6be6c6901cd
Antahkarana98 <Antahkarana98@github.com>
2023-04-22 18:51:21 -0500

    logica para carrousel movimiento con dragging y con los botones

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 import { apiQuery } from './api.js';
-// import { showProducts } from './showProducts.js';
+
 import { showStars } from './ui.js';
 import { ratingCalc, formattTags } from './functions.js';
+import { carousel } from './carousel.js';
 
 (function () {
 
   document.addEventListener('DOMContentLoaded', showProducts);
+
+  let carrusel = false;
 
   async function showProducts(){
     const products = await apiQuery();
@@ -25,8 +28,9 @@ import { ratingCalc, formattTags } from './functions.js';
           <div class="card-img">
             <img
             src="${url}"
-            alt=""
+            alt="product omage"
             class="img-card"
+            draggable="false"
             >
             <a href="#" class="button-card">Add to cart</a>
           </div>
@@ -57,76 +61,8 @@ import { ratingCalc, formattTags } from './functions.js';
       spanRating.textContent = `(${rating})`;
 
       ratingElement.appendChild(spanRating);
-
     });
+    carousel();
   }
-
-
-  const carousel = document.querySelector('.carousel');
-  const btnArrows = document.querySelectorAll('.wrapper i');
-  const firstCardWidth = document.querySelector('.card2').offsetWidth;
-  const carouselChildrens = [...carousel.children];
-
-  let isDraging = false, startX, starScrollLeft;
-
-  // obtine el numero de card que se adptan al carrusel
-  let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
-
-  // agrega las card al inicio del carrusel para el loop infinito
-  carouselChildrens.slice(-cardPerView).reverse().forEach(card =>{
-    carousel.insertAdjacentHTML('afterbegin', card.outerHTML);
-  })
-
-  // agrega las card al final del carrusel para el loop infinito
-  carouselChildrens.slice(0, cardPerView).forEach(card =>{
-    carousel.insertAdjacentHTML('beforeend', card.outerHTML);
-  })
-
-  // agregar el evento a los botoones del scroll
-  btnArrows.forEach(btn => {
-    btn.addEventListener('click', () => {
-      carousel.scrollLeft += btn.id === 'left' ? -firstCardWidth : firstCardWidth;
-    })
-  });
-
-  const dragStart = (e) => {
-    isDraging = true;
-    carousel.classList.add('dragging');
-    //Guarda la posicion inicial del mouse y del scroll del carrusel
-    startX = e.pageX;
-    starScrollLeft = carousel.scrollLeft;
-  }
-
-  const dragging = (e) => {
-    if(!isDraging) return; //si draggring es falso lo retorna
-    // actualiza el scroll del carrusel basado en el movimiento del cursor
-    carousel.scrollLeft = starScrollLeft - (e.pageX - startX);
-  }
-
-  const dragEnd = () => {
-    isDraging = false;
-    carousel.classList.remove('dragging');
-  }
-
-  const infinteCarousel = () => {
-    // si el scroll llega al inicio lo devuelve al final
-    // se puso 40 para poder detectar el inicio ya que no llega completamente a 0
-    if(carousel.scrollLeft === 40){
-      carousel.classList.add('no-transition');
-      carousel.scrollLeft = carousel.scrollWidth - (2 * carousel.offsetWidth);
-      carousel.classList.remove('no-transition');
-    }
-    // si el scroll llega al final lo devuelve al inicio
-    else if(Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth){
-      carousel.classList.add('no-transition');
-      carousel.scrollLeft = carousel.offsetWidth;
-      carousel.classList.remove('no-transition');
-    }
-  }
-
-  carousel.addEventListener('mousedown', dragStart);
-  carousel.addEventListener('mousemove', dragging);
-  carousel.addEventListener('mouseup', dragEnd);
-  carousel.addEventListener('scroll', infinteCarousel);
 
 })();
